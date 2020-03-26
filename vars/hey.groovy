387d0f92efe47339609de178751ed8680fd198e5
kapetro <62589747+kapetro@users.noreply.github.com>
2020-03-26 08:35:50 +0100

    Update hey.groovy

--- vars/hey.groovy
+++ vars/hey.groovy
@@ -2,5 +2,5 @@
 
 def call( Map map = [:], Map params = [:], String nesto = "") {
   echo "Params su ${params}"
-  echo "Ime je ${map.ime1}, a lista ${map.lista1}, a jos ${nesto}"
+  echo " jos ${nesto}"
 }
